067f3199f4fbcbfe574d73c36307ab8c2b378dc6
Diego Chacon <diegdarch@hotmail.com>
2026-05-14 14:44:29 +0200

    Fix: Remove ignored files and add .gitignore file

--- ejercicios/funcs_libc/ft_atoi.c
+++ ejercicios/funcs_libc/ft_atoi.c
@@ -6,37 +6,33 @@
 /*   By: dichacon <dichacon@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 12:20:47 by dichacon          #+#    #+#             */
-/*   Updated: 2026/05/13 17:13:58 by dichacon         ###   ########.fr       */
+/*   Updated: 2026/05/14 12:51:19 by dichacon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <stdio.h>
 
 int	ft_atoi(const char *nptr)
 {
-	size_t	i;
-	size_t	result;
-	int	sign;
+	size_t		i;
+	long		result;
+	int			sign;
 
 	i = 0;
 	result = 0;
-//	while (nptr[i] > 32 && nptr[i] < 127 && nptr[i] != '\0')
-	while (nptr[i] != '\0')
+	sign = 1;
+	while (((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ') && nptr[i] != '\0')
+		i++;
+	if (nptr[i] == '+')
+		i++;
+	else if (nptr[i] == '-')
 	{
-		if (nptr[i] >= 9 || nptr[i] == 13)
-		{
-			i++;
-			continue ;
-		}
-		if (nptr[i] == '+')i
-		{
-			i++;
-			continue ;
-		}
-		else if (!(nptr[i] >= '0') || !(nptr[i] <= '9'))
-			break ;
-		result = (result * 10) + (nptr[i] - '0');
+		sign = -1;
 		i++;
 	}
-	return (result);
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = (result * 10) + (nptr[i] - '0');
+		i++;	
+	}
+	return (result * sign);
 }
