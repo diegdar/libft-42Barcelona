2124c1e271d063f25100b303d085cc6eb549344b
diegdar <diegdarch@hotmail.com>
2026-05-15 18:47:22 +0200

    funcion ft_strdup.c

--- funcs_libc/ft_strdup.c
+++ funcs_libc/ft_strdup.c
@@ -6,12 +6,22 @@
 /*   By: dichacon <dichacon@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 11:57:04 by dichacon          #+#    #+#             */
-/*   Updated: 2026/05/15 11:57:43 by dichacon         ###   ########.fr       */
+/*   Updated: 2026/05/15 18:27:15 by dichacon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strdup(const char *s)
 {
-	
+	char	*copy;
+	size_t	len;
+
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s) + 1;
+	copy = malloc(len);
+	if (!copy)
+		return (NULL);
+	return (ft_memcpy(copy, s, len));
 }
--- mains/main_ft_strdup.c
+++ mains/main_ft_strdup.c
@@ -1,21 +1,46 @@
 #include "libft.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 static void test_fn(const char *s, char *test_name)
 {
-	char	*s_copy;
+	char	*copy1;
+	char	*copy2;
 
-	s_copy = ft_strdup(s);
+	printf("\n----- Test: %s ----", test_name);
+    copy1 = strdup(s);
+	copy2 = ft_strdup(s);
 
-	if (s_copy)
-	{
-		printf("\n✅ Se copio correctamente el string");
-		printf("\nString fuente> puntero: %p, valor: %s\n", s, *s);
-		printf("\nString copia> puntero: %p, valor: %s\n", s_copy, *s_copy);
-	}
-	else
-		printf("❌ Error: hubo un error al tratar de hacer la copia");
+    if (!copy1)
+    {       
+	    printf("copy1 devuelve un valor NULL");
+        free(copy1);
+        free(copy2);
+        return ;
+    }
+
+    if (!copy2)
+    {       
+	    printf("copy2 devuelve un valor NULL");
+        free(copy1);
+        free(copy2);
+        return ;
+    }
+
+	printf("\nString fuente> puntero: %p, valor: %s\n", s, s);
+
+	printf("\n>> comportamiento de strdup():");
+	printf("\nString copia> puntero: %p, valor: %s\n", copy1, copy1);
+
+	printf("\n>> comportamiento de ft_strdup():");
+	printf("\nString copia> puntero: %p, valor: %s\n", copy2, copy2);
+
+    printf("\n ======== Fin del Test ==============");
+
+    free(copy1);
+    free(copy2);
 } 
 
 int	main(void)
@@ -28,6 +53,9 @@ int	main(void)
 
 	s = "bar";
 	test_fn(s, "paso de un string de 3 bytes");
+
+	s = "";
+	test_fn(s, "Que pasa si se pasa un valor vacio?");
 
 	printf("\n=========================================\n");
 	return (0);
